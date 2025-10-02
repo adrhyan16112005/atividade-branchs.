17f664d10f63b7d45e18cf3ae87f787031027e12
adrhyan16112005 <adrhyanviniciusnunesflores@gmail.com>
2025-10-02 15:00:01 -0400

    Adiciona função despedida na funcionalidade 1

--- funcionalidade1.py
+++ funcionalidade1.py
@@ -2,3 +2,10 @@
 
 def saudacao():
     return "Olá, bem-vindo à Funcionalidade 1!"
+# funcionalidade1.py
+
+def saudacao():
+    return "Olá, bem-vindo à Funcionalidade 1!"
+
+def despedida():
+    return "Até mais, da Funcionalidade 1!"
